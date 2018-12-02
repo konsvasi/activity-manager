75f9d40e4d7d7dc13d06f26e69d35782e70e3b26
konsvasi <konsvass@outlook.com>
2018-12-02 18:38:39 +0200

    added particle-js to login page, without success WIP

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import React, { Component } from 'react';
-import SignInRegister from './Components/SignInRegister';
-import 'bulma/css/bulma.css';
+import React, { Component } from "react";
+import Particles from "react-particles-js";
+import SignInRegister from "./Components/SignInRegister";
+import "bulma/css/bulma.css";
+import "./App.css";
+
+const particlesConfig = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className="particles" params={particlesConfig} />
         <section className="section">
           <div className="container">
             <div className="columns">
-              <div className="column"></div>
+              <div className="column" />
               <div className="column is-one-third">
                 <SignInRegister />
               </div>
-              <div className="column"></div>
+              <div className="column" />
             </div>
           </div>
         </section>
